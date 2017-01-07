bf378c460b715d219f2833ee5d0fca33b0136c02
Francis Jose Alapppat <FRANCISJOSE.ALAPPAT@aexp.com>
2017-01-06 18:19:48 -0700

    assignment-3 - validation

--- assignment-3/app.js
+++ assignment-3/app.js
@@ -12,22 +12,25 @@
     function NarrowItDownController($http, MenuSearchService) {
         var narrow = this;
         narrow.searchTerm = "";
+        narrow.found = [];
 
         narrow.getItems = function (searchTerm) {
-            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-            promise.then(function (response) {
-                //store the filtered data in the scope variable
-                narrow.found = response;
-                console.log('narrow.found length=',response.length);
-            }).catch(function (error) {
-                console.log("Error while getting data from webservice!",error);
-            })
+            //prevent unwanted requests by check
+            if (searchTerm) {
+                var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+                promise.then(function (response) {
+                    //store the filtered data in the scope variable
+                    narrow.found = response;
+                }).catch(function (error) {
+                    console.log("Error while getting data from webservice!", error);
+                })
+            }
         };
 
         narrow.removeItemAtIndex = function (index) {
             narrow.found.splice(index,1);
         };
-        
+
     }
 
     MenuSearchService.$inject = ['$http', '$filter'];
@@ -35,20 +38,14 @@
         var service = this;
 
         service.getMatchedMenuItems = function (searchTerm) {
-            console.log('search for :',searchTerm);
             return $http({
                 method: "GET",
                 url: "https://davids-restaurant.herokuapp.com/menu_items.json"
             }).then(function (result) {
-                // var temp = result.data.menu_items;
-                // console.log('temp',temp);
-                // process result and only keep items that match
                 var foundItems = result.data.menu_items.filter(function (ele) {
                      if(ele.description.indexOf(searchTerm) !== -1)
                          return ele;
                 });
-                console.log('found items',foundItems);
-                console.log('found items length=',foundItems.length);
                 // return processed items
                 return foundItems;
             });
@@ -60,38 +57,49 @@
             restrict: 'EA',
             templateUrl: "itemsloaderindicator.template.html",
             scope:{
-                displayItems: '=',
-                onRemove: '&'
+                displayItems: '<',
+                onRemove: '&',
+                searchTerm: '<'
             },
             controller: ItemLoaderCtrl,
             controllerAs: 'narrowItCtrl',
-            bindToController: true
-            // link: ItemLoaderLink
+            bindToController: true,
+            link: ItemLoaderLink
         };
         return ddo;
     }
 
     function ItemLoaderCtrl() {
-        var itemLoader = this;
+        var narrowItCtrl = this;
 
-        // itemLoader.itemList = function () {
-        //     for(var i=0; i< narrowItCtrl.found.length; i++){
-        //         var name = narrowItCtrl.found[i].name;
-        //
-        //     }
-        // }
+        narrowItCtrl.showError = function () {
+            return (narrowItCtrl.displayItems.length == 0) ? true:false;
+        }
 
     }
 
+    function ItemLoaderLink(scope, element, attributes) {
+        scope.$watch('narrowItCtrl.showError()',function (newVal,oldVal) {
+            console.log("Old value: ", oldVal);
+            console.log("New value: ", newVal);
+            if(newVal == true && attributes){
+                displayError();
+            }else{
+                hideError();
+            }
+        });
+        function displayError() {
+            var warningElem = element.find("div");
+            console.log('displayError:',warningElem);
+            warningElem.css('display', 'block');
+        }
 
-    function ItemLoaderLink(scope, element, attributes, controller) {
-        console.log("Link Scope=",scope);
-        console.log("Controller Instance is ",controller);
-        console.log("Element is ",element);
 
-        // scope.$watch(list.c)
+        function hideError() {
+            var warningElem = element.find("div");
+            console.log('hideError:',warningElem);
+            warningElem.css('display', 'none');
+        }
     }
-
-
 
 })();
